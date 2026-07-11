5814b19bf8a09e4efc8bc8f5ccb39f5950454ec0
Yoku <yoku3103@gmail.com>
2026-07-11 14:36:16 +0900

    update by yoku

--- test copy 2.tsx
+++ test copy 2.tsx
@@ -1,5 +1,4 @@
-PageNumbers を app/_components/PageNumbers/index.tsx に移植
-app/blog/page.tsx に接続
 
 
-今度はapp\_components\PageNumbers\index.tsxを作りたい。この旧サイトのコードを元にAppRouter、TypeScript、tailwind(必要に応じてmodule.css)での修正例を作ってほしい。
+
+今度はapp\blog\[slug]\page.tsxを作りたい。この旧サイトのコードを元にAppRouter、TypeScript、tailwind(必要に応じてmodule.css)での修正例を作ってほしい。
